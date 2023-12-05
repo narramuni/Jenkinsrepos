e772bb02574fcaf3bbc89558cb0fe2c56a78aa00
narramuni <nmspavan@gmail.com>
2023-12-05 13:29:23 +0530

    Second Commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JenkinsDemo.Models;
+namespace JenkinsDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ProductModel model = new ProductModel();    
+            model.ProductID = 1;
+            model.ProductName = "Shirt";
+            return View();
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using 
+namespace JenkinsDemo.Models
+{
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+    }
+}
